ebf3d9e758b66caf26c398d954e1209bf082c711
José Tomás Albornoz <jojo@eljojo.net>
2014-06-02 14:54:24 +0200

    Refactored MMU

--- lib/mimey/mmu.rb
+++ lib/mimey/mmu.rb
@@ -6,34 +6,51 @@ module Mimey
     # Initializes the memory areas
     def initialize()
       @internal_memory = Array.new(8192, 0x00)
-      @zram = Array.new(8192, 0x00)
+      @zram = Array.new(128, 0x00)
+      @external_memory = Array.new(32768, 0x00)
+
       @word_accessor = WordAccessor.new(self)
+      @rom_offset = 0x4000
+      @ram_offset = 0
+
+      @ie = 0
     end
 
     # Reads a byte from to the different memory areas
     def [](i)
       case i
-      when 0x0000..0x7FFF
+      # ROM BANK 0
+      when 0x0000..0x0FFF
         @rom[i]
-      when 0xC000..0xDFFF, 0xE000..0xFDFF
+      when 0x1000..0x3FFF
+        @rom[i]
+      # ROM BANK 1
+      when 0x4000..0x7FFF
+        @rom[@rom_offset + (i & 0x3FFF)]
+      # VRAM
+      when 0x8000..0x9FFF
+        @gpu.vram[i & 0x1FFF]
+      # External RAM
+      when 0xA000..0xBFFF
+        @external_memory[@ram_offset + (i & 0x1FFF)]
+      # Work RAM and echo
+      when 0xC000..0xFDFF
         @internal_memory[i & 0x1FFF]
-      when 0xF000..0xFFFF
-        case i & 0x0F00
-          # Zero-page
-        when 0xF00
-          if i >= 0xFF80
-            @zram[i & 0x7F]
-          else
-            # I/O control handling
-            case i & 0x00F0
-              # GPU (64 registers)
-            when 0x40, 0x50, 0x60, 0x70
-              # GPU.rb(addr)
-            else
-              0
-            end
-          end
-        end
+      when 0xFE00..0xFEFF
+        # return ((addr&0xFF)<0xA0) ? GPU._oam[addr&0xFF] : 0;
+        puts "OAM"
+        exit
+      when 0xFFFF
+        @ie
+      # Zero page
+      when 0xFF80..0xFFFF
+        @zram[i & 0x7F]
+      # GPU (64 registers)
+      when 0xFF40..0xFF7F
+        @gpu[i]
+      else
+        puts "trying to read unreachable memory: #{i} (0x#{i.to_s(16)})"
+        exit
       end
     end
 
@@ -45,26 +62,33 @@ module Mimey
     # Writes a byte to the different memory areas
     def []=(i, n)
       case i
-      when 0xC000..0xDFFF, 0xE000..0xFDFF
-        @internal_memory[i & 0x1FFF] = n
+      # VRAM
       when 0x8000..0x9FFF
         @gpu.vram[i & 0x1FFF] = n
-        @gpu.update_tile(i, n)
-      when 0xF000..0xFFFF
-        case i & 0x0F00
-          # Zero-page
-        when 0xF00
-          if i >= 0xFF80
-            @zram[i & 0x7F] = n
-          else
-            # I/O control handling
-            case i & 0x00F0
-              # GPU (64 registers)
-            when 0x40, 0x50, 0x60, 0x70
-              # GPU.wb(addr, val)
-            end
-          end
-        end
+        @gpu.update_tile(i & 0x1FFF, n)
+      # External RAM
+      when 0xA000..0xBFFF
+        @external_memory[@ram_offset + (i & 0x1FFF)] = n
+      # Work RAM and echo
+      when 0xC000..0xFDFF
+        @internal_memory[i & 0x1FFF] = n
+      when 0xFE00..0xFEFF
+        # if ((addr & 0xFF) < 0xA0) GPU._oam[addr & 0xFF] = val;
+        # GPU.updateoam(addr, val);
+        puts "OAM"
+        exit
+      when 0xFFFF
+        @ie
+      when 0xFF00..0xFF3F
+      # Zero page
+      when 0xFF80..0xFFFF
+        @zram[i & 0x7F] = n
+      # GPU (64 registers)
+      when 0xFF40..0xFF7F
+        @gpu[i] = n
+      else
+        puts "trying to update unreachable memory: #{i} (0x#{i.to_s(16)}) => #{n}"
+        exit
       end
     end
 
