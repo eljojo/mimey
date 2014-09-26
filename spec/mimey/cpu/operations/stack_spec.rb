6680991a79933a7a8613a1877bbd19d71c55dc0b
José Tomás Albornoz <jojo@eljojo.net>
2014-09-26 23:21:38 +0200

    Removed spec stuff. who needs specs anyways.

--- spec/mimey/cpu/operations/stack_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Mimey::CPU do
-  subject(:cpu) { Mimey::CPU.new(options) }
-  let(:options) { Mimey::CPU::DEFAULTS }
-
-  pending "implements PUSH_ methods correctly"
-  pending "implements POP_ methods correctly"
-end
